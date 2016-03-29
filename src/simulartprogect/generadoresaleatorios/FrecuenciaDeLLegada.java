bb3f0d3b713259c0daf598993838ef21b17be369
Any2016 <74014.linares@gmail.com>
2016-03-29 06:06:51 -0300

    Added files via upload

--- src/simulartprogect/generadoresaleatorios/FrecuenciaDeLLegada.java
+++ src/simulartprogect/generadoresaleatorios/FrecuenciaDeLLegada.java
@@ -5,16 +5,35 @@
  */
 package simulartprogect.generadoresaleatorios;
 
-import java.util.Random;
+import simulartprogect.generadoresaleatorios.DistribucionExponencial;
+import simulartprogect.modelo.Cliente.Tipo;
 
 /**
  *
- * @author Irene Argollo
+ * @author Andrea
  */
-public class FrecuenciaDeLLegada {
-    
-    public int siguienteTiempoDellegada() {
-        Random tiempoMilisegundos = new Random();
-        return tiempoMilisegundos.nextInt(100000);
+public class FrecuenciaDeLLegada{
+
+    DistribucionExponencial tiempo = new DistribucionExponencial();
+
+    @SuppressWarnings("ConvertToStringSwitch")
+    public int obtenerTiempo(Tipo cliente) {
+        //1 hora es equivalente a 60000
+        int media = 0;
+        int res;
+        if (cliente.equals(Tipo.DEPORTISTA)) {
+            media = 30000;
+        } else if (cliente.equals(Tipo.ESTUDIANTE)) {
+            media = 60000;
+        } else if (cliente.equals(Tipo.TURISTA_EXTRANGERO)) {
+            media = 30000;
+        } else if (cliente.equals(Tipo.TURISTA_NACIONAL)) {
+            media = 15000;
+        } else {
+            System.out.println("error");
+        }
+
+        return res = tiempo.obtenerDE(media);
+
     }
 }
